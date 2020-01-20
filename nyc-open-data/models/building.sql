b07e3648dbce067fb4d7cc0395673b8afde77fd2
kennylim <work@kennylim.com>
2020-01-20 08:39:09 +0530

    DA-7 	modified:   nyc-open-data/models/logfile
    	new file:   nyc-open-data/models/building.sql
    	modified:   nyc-open-data/models/logfile

---
+++ nyc-open-data/models/building.sql
@@ -0,0 +1,17 @@
+--nyc building
+
+
+SELECT bbl,
+       bin,
+       owner,
+       party_type,
+       strees_number,
+       street_name,
+       building_grade,
+       crime_rate_grade,
+       flood_zone,
+       nearest_fire_station,
+       nearest_police_station,
+       nearest_hospital
+
+-- acris_master
